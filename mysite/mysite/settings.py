d93b6559fef72dc183b3436410d94f0735b536f8
Ubuntu <esam@ubuntuEsaMakinen.foc4fkkb0slejiph2tcy50ubrh.fx.internal.cloudapp.net>
2020-03-10 15:28:59 +0000

    backuppi ennen galleriaa

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,12 +74,36 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': 'testdb',
+    #    'USER': 'esamakinen',
+     #   'PASSWORD': 'Kakkahaisee1!',
+      #  'HOST': 'localhost',
+       # 'PORT': '',
+  #  }    
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'django',
+        'PASSWORD': 'Kakkahaisee1!',
+        'HOST': 'localhost',
+        'PORT': '',
+    }    
+
+
 }
+
 
 
 # Password validation
